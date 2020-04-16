83e3b2f75eab64b1c8b80cb23b2166b0241cea57
zm <250081124@qq.com>
2020-04-16 19:52:30 +0800

    add

--- LeetCode/1-50题/main.go
+++ LeetCode/1-50题/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"go-common-master/app/admin/main/up/util/mathutil"
 )
 
 func main() {
 	//num1()
 	//num2()
 	//num3()
-	num7()
+	//num4()
+	num5()
+	//num7()
 }
 
 /*
@@ -202,6 +205,114 @@ func lengthOfLongestSubstring(s string) int {
 		}
 	}
 	return max
+}
+
+/*
+给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+
+请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+
+你可以假设 nums1 和 nums2 不会同时为空。
+
+示例 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+则中位数是 2.0
+示例 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+则中位数是 (2 + 3)/2 = 2.5
+
+*/
+
+func num4() {
+	fmt.Println("findMedianSortedArrays:", findMedianSortedArrays([]int{3}, []int{1, 2, 4, 5}))
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	var p1, p2, mid, ln1, ln2 int
+	var isodd bool
+	var sums []int
+	ln1 = len(nums1)
+	ln2 = len(nums2)
+	mid = (ln1 + ln2) / 2
+	if (ln1+ln2)%2 != 0 {
+		isodd = true
+	}
+	for p1+p2 <= ln1+ln2 {
+		if p1 < ln1 && p2 < ln2 {
+			if nums1[p1] < nums2[p2] {
+				sums = append(sums, nums1[p1])
+				p1++
+			} else {
+				sums = append(sums, nums2[p2])
+				p2++
+			}
+		} else if p1 >= ln1 {
+			sums = append(sums, nums2[p2:]...)
+			break
+		} else if p2 >= ln2 {
+			sums = append(sums, nums1[p1:]...)
+			break
+		}
+	}
+	if isodd {
+		return float64(sums[mid])
+	} else {
+		if mid != 0 {
+			return float64(sums[mid-1]+sums[mid]) / 2
+		} else {
+			return float64(sums[mid]+sums[mid+1]) / 2
+		}
+	}
+}
+
+/*
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+示例 1：
+
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+示例 2：
+
+输入: "cbbd"
+输出: "bb"
+
+*/
+
+func num5() {
+	fmt.Println("longestPalindrome:", longestPalindrome("babad"))
+}
+
+func longestPalindrome(s string) string {
+	if len(s) < 2 {
+		return s
+	}
+	var start, end int
+	for i, _ := range s {
+		l1 := expandAroundCenter(s, i, i)
+		l2 := expandAroundCenter(s, i, i+1)
+		max := mathutil.Max(l1, l2)
+		if max > end-start {
+			start = i - (max-1)/2
+			end = i + max/2
+		}
+	}
+	return s[start : end+1]
+}
+
+func expandAroundCenter(s string, l, r int) (length int) {
+	for l >= 0 && r < len(s) && s[l] == s[r] {
+		l--
+		r++
+	}
+	return r - l - 1
 }
 
 /*
