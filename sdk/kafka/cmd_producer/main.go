0c6f6d1a9bc0d28d14d2626bb26b2c95c221673c
zhangmeng <zhangmeng04@qutoutiao.net>
2021-08-12 14:01:57 +0800

    add

--- sdk/kafka/cmd_producer/main.go
+++ sdk/kafka/cmd_producer/main.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"learning-note/sdk/kafka/producer"
 	"strings"
-	"time"
+	"sync"
 )
 
 var topic = flag.String("topic", "", "发送的topic")
 var value = flag.String("value", "", "发送的内容byte")
 var brokers = flag.String("brokers", "", "brokers逗号分隔")
 var count = flag.Int("count", 1, "发送条数")
+
+var wg sync.WaitGroup
 
 //启动命令参考： ./cmd_producer -topic topic  -brokers 192.168.194.128:9092
 func main() {
@@ -22,17 +24,20 @@ func main() {
 	p, err := producer.InitProducer(&producer.ProducerConf{
 		Addrs: strings.Split(*brokers, ","),
 		SuccessFunc: func(info *producer.CallbackInfo) {
-			fmt.Printf("send success: ;topic:%s;msg:%s;offset:%d", info.ProducerMessage.Topic, info.ProducerMessage.Value, info.ProducerMessage.Offset)
+			fmt.Printf("send success: ;topic:%s;msg:%s;offset:%d;partition:%d\n", info.ProducerMessage.Topic, info.ProducerMessage.Value, info.ProducerMessage.Offset, info.ProducerMessage.Partition)
+			wg.Done()
 		},
 		ErrorFunc: func(info *producer.CallbackInfo) {
-			fmt.Printf("send err:%v ;topic:%s;msg:%s;offset:%d", info.Err, info.ProducerMessage.Topic, info.ProducerMessage.Value, info.ProducerMessage.Offset)
+			fmt.Printf("send err:%v ;topic:%s;msg:%s;offset:%d;partition:%d\n", info.Err, info.ProducerMessage.Topic, info.ProducerMessage.Value, info.ProducerMessage.Offset, info.ProducerMessage.Partition)
+			wg.Done()
 		},
 	})
 	if err != nil {
 		panic(err)
 	}
-
-	for i := 0; i < *count; i++ {
+	wg.Add(*count)
+	i := 0
+	for i = 0; i < *count; i++ {
 		//单条耗时：183.916µs
 		p.PushMsg(&producer.ProducerMessage{
 			Topic: *topic,
@@ -40,7 +45,7 @@ func main() {
 		})
 	}
 
-	time.Sleep(time.Second)
-	fmt.Println("execute success.")
+	wg.Wait()
+	fmt.Println("execute success.num:", i)
 
 }
