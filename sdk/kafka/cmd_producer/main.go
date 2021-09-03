484d6a4d8f34f40390a126d5d3afef55a3655569
zhangmeng <zhangmeng04@qutoutiao.net>
2021-09-03 17:29:58 +0800

    add

--- sdk/kafka/cmd_producer/main.go
+++ sdk/kafka/cmd_producer/main.go
@@ -6,12 +6,14 @@ import (
 	"learning-note/sdk/kafka/producer"
 	"strings"
 	"sync"
+	"sync/atomic"
 )
 
 var topic = flag.String("topic", "", "发送的topic")
 var value = flag.String("value", "", "发送的内容byte")
 var brokers = flag.String("brokers", "", "brokers逗号分隔")
 var count = flag.Int("count", 1, "发送条数")
+var producerCount = flag.Int("producer_count", 1, "生产者数")
 
 var wg sync.WaitGroup
 
@@ -19,33 +21,45 @@ var wg sync.WaitGroup
 func main() {
 	flag.Parse()
 
-	fmt.Printf("params: topic:%s, brokers:%s, count:%d, value:%s \n", *topic, *brokers, *count, *value)
+	fmt.Printf("params: topic:%s, brokers:%s, count:%d, value:%s ,producer_count:%d \n", *topic, *brokers, *count, *value, *producerCount)
 
-	p, err := producer.InitProducer(&producer.ProducerConf{
-		Addrs: strings.Split(*brokers, ","),
-		SuccessFunc: func(info *producer.CallbackInfo) {
-			fmt.Printf("send success: ;topic:%s;msg:%s;offset:%d;partition:%d\n", info.ProducerMessage.Topic, info.ProducerMessage.Value, info.ProducerMessage.Offset, info.ProducerMessage.Partition)
-			wg.Done()
-		},
-		ErrorFunc: func(info *producer.CallbackInfo) {
-			fmt.Printf("send err:%v ;topic:%s;msg:%s;offset:%d;partition:%d\n", info.Err, info.ProducerMessage.Topic, info.ProducerMessage.Value, info.ProducerMessage.Offset, info.ProducerMessage.Partition)
-			wg.Done()
-		},
-	})
-	if err != nil {
-		panic(err)
-	}
-	wg.Add(*count)
-	i := 0
-	for i = 0; i < *count; i++ {
-		//单条耗时：183.916µs
-		p.PushMsg(&producer.ProducerMessage{
-			Topic: *topic,
-			Value: []byte(*value),
+	producers := make([]*producer.KafkaProducer, 0, *producerCount)
+	for i := 0; i < *producerCount; i++ {
+		p, err := producer.InitProducer(&producer.ProducerConf{
+			Addrs: strings.Split(*brokers, ","),
+			SuccessFunc: func(info *producer.CallbackInfo) {
+				//fmt.Printf("send success: ;topic:%s;msg:%s;offset:%d;partition:%d\n", info.ProducerMessage.Topic, info.ProducerMessage.Value, info.ProducerMessage.Offset, info.ProducerMessage.Partition)
+				wg.Done()
+			},
+			ErrorFunc: func(info *producer.CallbackInfo) {
+				fmt.Printf("send err:%v ;topic:%s;msg:%s;offset:%d;partition:%d\n", info.Err, info.ProducerMessage.Topic, info.ProducerMessage.Value, info.ProducerMessage.Offset, info.ProducerMessage.Partition)
+				wg.Done()
+			},
 		})
+		if err != nil {
+			panic(err)
+		}
+		producers = append(producers, p)
+	}
+
+	fmt.Printf("producers_num:%d \n", len(producers))
+
+	wg.Add(*count * *producerCount)
+	var n int32 = 0
+	for index := range producers {
+		go func(index int) {
+			for i := 0; i < *count; i++ {
+				//单条耗时：183.916µs
+				producers[index].PushMsg(&producer.ProducerMessage{
+					Topic: *topic,
+					Value: []byte(*value),
+				})
+				atomic.AddInt32(&n, 1)
+			}
+		}(index)
 	}
 
 	wg.Wait()
-	fmt.Println("execute success.num:", i)
+	fmt.Println("execute success.num:", n)
 
 }
